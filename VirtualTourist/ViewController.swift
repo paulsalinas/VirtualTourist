61cd2942b144e9dca5acbd340097d6cd35c828f0
Paul <paul.salina@gmail.com>
2016-03-01 22:51:28 -0500

    use nskeyedarchiver for default state instead of nsuserdefaults

--- VirtualTourist/ViewController.swift
+++ VirtualTourist/ViewController.swift
@@ -25,23 +25,7 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, MKMapViewDe
         
         mapView.delegate = self
         
-        if NSUserDefaults.standardUserDefaults().boolForKey("savedAppState") {
-            let latitudeCenter = NSUserDefaults.standardUserDefaults().doubleForKey("latitudeCenter")
-            let longitudeCenter = NSUserDefaults.standardUserDefaults().doubleForKey("longitudeCenter")
-            let longitudeSpan = NSUserDefaults.standardUserDefaults().doubleForKey("longitudeSpan")
-            let latitudeSpan = NSUserDefaults.standardUserDefaults().doubleForKey("latitudeSpan")
-            
-            let centerCoordinate =  CLLocationCoordinate2D(latitude: latitudeCenter, longitude: longitudeCenter)
-            let span = MKCoordinateSpan(latitudeDelta: latitudeSpan, longitudeDelta: longitudeSpan)
-            
-            let altitude = NSUserDefaults.standardUserDefaults().doubleForKey("altitude")
-            let pitch = NSUserDefaults.standardUserDefaults().objectForKey("pitch") as! CGFloat
-            let heading = NSUserDefaults.standardUserDefaults().doubleForKey("heading")
-            
-            mapView.region = MKCoordinateRegion(center: centerCoordinate, span: span)
-            mapView.setCamera(MKMapCamera(lookingAtCenterCoordinate: centerCoordinate, fromDistance: altitude, pitch: pitch, heading: heading), animated: false)
-        }
-
+        restoreMapState(false)
     }
     
     // MARK: - Gesture Handler Functions
@@ -60,7 +44,6 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, MKMapViewDe
         let annotation = (recognizer.view as! MKPinAnnotationView).annotation!
         print(annotation.coordinate)
     }
-    
     
     // MARK: - Map Delegate Functions
     
@@ -92,22 +75,66 @@ class ViewController: UIViewController, UIGestureRecognizerDelegate, MKMapViewDe
     
 
     func mapView(mapView: MKMapView, regionDidChangeAnimated animated: Bool) {
-
-        let region = mapView.region
-
-        NSUserDefaults.standardUserDefaults().setDouble(region.center.latitude, forKey: "latitudeCenter")
-        NSUserDefaults.standardUserDefaults().setDouble(region.center.longitude, forKey: "longitudeCenter")
-        
-        NSUserDefaults.standardUserDefaults().setDouble(region.span.latitudeDelta, forKey: "latitudeSpan")
-        NSUserDefaults.standardUserDefaults().setDouble(region.span.longitudeDelta, forKey: "longitudeSpan")
-        NSUserDefaults.standardUserDefaults().setBool(true, forKey: "savedAppState")
-        
-        NSUserDefaults.standardUserDefaults().setDouble(mapView.camera.altitude, forKey: "altitude")
-        NSUserDefaults.standardUserDefaults().setObject(mapView.camera.pitch, forKey: "pitch")
-        NSUserDefaults.standardUserDefaults().setDouble(mapView.camera.heading, forKey: "heading")
-
+        saveMapState()
     }
     
-
+    // MARK: - Save the zoom level helpers
+    
+    // Here we use the same filePath strategy as the Persistent Master Detail
+    // A convenient property
+    var filePath : String {
+        let manager = NSFileManager.defaultManager()
+        let url = manager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first! as NSURL
+        return url.URLByAppendingPathComponent("mapRegionArchive").path!
+    }
+    
+    func saveMapState() {
+        
+        // Place the "center" and "span" of the map into a dictionary
+        // The "span" is the width and height of the map in degrees.
+        // It represents the zoom level of the map.
+        
+        let dictionary = [
+            "latitude" : mapView.region.center.latitude,
+            "longitude" : mapView.region.center.longitude,
+            "latitudeDelta" : mapView.region.span.latitudeDelta,
+            "longitudeDelta" : mapView.region.span.longitudeDelta,
+            "altitude": mapView.camera.altitude,
+            "pitch": mapView.camera.pitch,
+            "heading": mapView.camera.heading
+        ]
+        
+        // Archive the dictionary into the filePath
+        NSKeyedArchiver.archiveRootObject(dictionary, toFile: filePath)
+    }
+    
+    func restoreMapState(animated: Bool) {
+        
+        // if we can unarchive a dictionary, we will use it to set the map back to its
+        // previous center and span
+        if let regionDictionary = NSKeyedUnarchiver.unarchiveObjectWithFile(filePath) as? [String : AnyObject] {
+            
+            let longitude = regionDictionary["longitude"] as! CLLocationDegrees
+            let latitude = regionDictionary["latitude"] as! CLLocationDegrees
+            let center = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+            
+            let longitudeDelta = regionDictionary["latitudeDelta"] as! CLLocationDegrees
+            let latitudeDelta = regionDictionary["longitudeDelta"] as! CLLocationDegrees
+            let span = MKCoordinateSpan(latitudeDelta: latitudeDelta, longitudeDelta: longitudeDelta)
+            
+            let savedRegion = MKCoordinateRegion(center: center, span: span)
+            
+            print("lat: \(latitude), lon: \(longitude), latD: \(latitudeDelta), lonD: \(longitudeDelta)")
+            
+            mapView.setRegion(savedRegion, animated: animated)
+            
+            let altitude = regionDictionary["altitude"] as! CLLocationDistance
+            let pitch = regionDictionary["pitch"] as! CGFloat
+            let heading = regionDictionary["heading"] as! CLLocationDirection
+            
+            let savedCamera = MKMapCamera(lookingAtCenterCoordinate: center, fromDistance: altitude, pitch: pitch, heading: heading)
+            mapView.setCamera(savedCamera, animated: animated)
+        }
+    }
 }
 
